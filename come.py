d3bf32e2785c4dbf46b5b5193c71ef85b6933be3
newbalancem5 <newbalancem5@gmail.com>
2019-10-13 19:40:39 +0300

    upd

--- come.py
+++ come.py
@@ -1,18 +1,14 @@
 import time
 import RPi.GPIO as GPIO 
+import os
 
 def RCtime(RCpin):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RCpin, GPIO.IN)
     GPIO.wait_for_edge(RCpin, GPIO.RISING)
     signal = GPIO.input(RCpin)
-    try:
-        exit()
-    except:
-        print("Нашелся")
-
 while True:
     RCtime(4)
     time.sleep(2)    
-    
-        
+    os.system("omxplayer -o local mvd.mp4")
+      
